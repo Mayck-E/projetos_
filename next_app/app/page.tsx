3e0fe7dd6be5681d5d61961b1385f94dea7ec716
May_Edu <mayck22.03@hotmail.com>
2024-05-03 08:09:00 -0300

    .

---
+++ next_app/app/page.tsx
@@ -0,0 +1,20 @@
+
+
+export default function Home() {
+    return (
+        <main className="flex min-h-screen flex-row items-center p-24">
+            <nav className="flex flex-col mr-10">
+                <a href="">01</a>
+                <a href="">02</a>
+                <a href="">03</a>
+                <a href="">04</a>
+            </nav>
+            <div>
+                <h1 className="text-3xl font-black ">Testando um app Next.js</h1>
+                <p style={{ fontSize: 20 }}>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum quae delectus ipsum architecto beatae rerum veniam enim, aspernatur placeat esse maxime id quis corrupti voluptas qui, quidem excepturi incidunt culpa?</p>
+            </div>
+
+
+        </main>
+    );
+}
